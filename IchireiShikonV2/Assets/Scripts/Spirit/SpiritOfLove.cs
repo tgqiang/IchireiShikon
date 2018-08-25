706d0459de13ac32bf7f21e9c5d4fd9248097482
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2018-08-25 19:44:01 +0800

    Implemented Spirit of Love spirit-effect in scripting

--- IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfLove.cs
+++ IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfLove.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class SpiritOfLove : Spirit {
 
+    const int AREAS_OF_EFFECT_MAX = 17;
+    const int AREAS_OF_EFFECT_PER_LEVEL = 4;
+
+    [SerializeField]
+    SpriteRenderer[] areaOfEffect;
+
     public override void Start() {
         base.Start();
+        if (areaOfEffect.Length != AREAS_OF_EFFECT_MAX) {
+            throw new System.Exception("Array of areas of effect is of incorrect length.");
+        }
         spiritType = CustomEnums.SpiritType.LOVE;
     }
 
-    public override void TriggerEffect() {
+    public override void ShowAreaOfEffect() {
+        for (int i = 0; i <= spiritLevel * AREAS_OF_EFFECT_PER_LEVEL; i++) {
+            areaOfEffect[i].enabled = true;
+        }
+    }
 
+    public override void HideAreaOfEffect() {
+        for (int i = 0; i <= spiritLevel * AREAS_OF_EFFECT_PER_LEVEL; i++) {
+            areaOfEffect[i].enabled = false;
+        }
+    }
+
+    public override void TriggerEffect() {
+        Tile[][] tileMap = Tile.gameLevel.levelData.tileMap;
+        Vector2Int mapBounds = Tile.gameLevel.levelData.mapBounds;
+        Vector2Int currentCoords = CurrentLocation.tileCoords;
+
+        for (int i = 0; i <= spiritLevel * AREAS_OF_EFFECT_PER_LEVEL; i++) {
+            areaOfEffect[i].GetComponent<SpiritOfLoveEffect>().ExertEffect(currentCoords, mapBounds, tileMap);
+        }
+
+        DegradeAfterTrigger();
     }
 }
---
+++ IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfLoveEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritOfLoveEffect : SpiritEffect {
+
+    public override void PerformEffect(Tile target) {
+        if (!target.IsVacant()) {
+            target.objectOnTile.Purify();
+        }
+
+        target.Purify();
+    }
+}
