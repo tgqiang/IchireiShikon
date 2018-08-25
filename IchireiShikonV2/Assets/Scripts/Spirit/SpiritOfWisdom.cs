35e97c0f9a24aff27860d15bf914a5e187a64845
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2018-08-25 13:27:51 +0800

    Reworked Spirit of Wisdom effect-trigger mechanism

--- IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfWisdom.cs
+++ IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfWisdom.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class SpiritOfWisdom : Spirit {
 
-    const int AREAS_OF_EFFECT_MAX = 13;
+    const int AREAS_OF_EFFECT_MAX = 17;
+    const int AREAS_OF_EFFECT_PER_LEVEL = 4;
 
     [SerializeField]
     SpriteRenderer[] areaOfEffect;
@@ -18,81 +19,26 @@ public class SpiritOfWisdom : Spirit {
     }
 
     public override void ShowAreaOfEffect() {
-        switch (spiritLevel) {
-            case 4:
-            case 3:
-                for (int i = 5; i < 5 + (spiritLevel - 2) * 4; i++) {
-                    areaOfEffect[i].enabled = true;
-                }
-                goto case 2;
-            case 2:
-            case 1:
-                for (int i = 0; i <= spiritLevel * 2; i++) {
-                    areaOfEffect[i].enabled = true;
-                }
-                break;
-            default:
-                break;
+        for (int i = 0; i <= spiritLevel * AREAS_OF_EFFECT_PER_LEVEL; i++) {
+            areaOfEffect[i].enabled = true;
         }
     }
 
     public override void HideAreaOfEffect() {
-        switch (spiritLevel) {
-            case 4:
-            case 3:
-                for (int i = 5; i < 5 + (spiritLevel - 2) * 4; i++) {
-                    areaOfEffect[i].enabled = false;
-                }
-                goto case 2;
-            case 2:
-            case 1:
-                for (int i = 0; i <= spiritLevel * 2; i++) {
-                    areaOfEffect[i].enabled = false;
-                }
-                break;
-            default:
-                break;
+        for (int i = 0; i <= spiritLevel * AREAS_OF_EFFECT_PER_LEVEL; i++) {
+            areaOfEffect[i].enabled = false;
         }
     }
 
     public override void TriggerEffect() {
         Tile[][] tileMap = Tile.gameLevel.levelData.tileMap;
         Vector2Int mapBounds = Tile.gameLevel.levelData.mapBounds;
-        Vector2Int coords = CurrentLocation.tileCoords;
+        Vector2Int currentCoords = CurrentLocation.tileCoords;
 
-        switch (spiritLevel) {
-            case 4:
-                if (coords.x - 2 >= 0 && tileMap[coords.x - 2][coords.y] != null)
-                    tileMap[coords.x - 2][coords.y].PurifyWithBarrier();
-                if (coords.x + 2 < mapBounds.x && tileMap[coords.x + 2][coords.y] != null)
-                    tileMap[coords.x + 2][coords.y].PurifyWithBarrier();
-                if (coords.y - 2 >= 0 && tileMap[coords.x][coords.y - 2] != null)
-                    tileMap[coords.x][coords.y - 2].PurifyWithBarrier();
-                if (coords.y + 2 < mapBounds.y && tileMap[coords.x][coords.y + 2] != null)
-                    tileMap[coords.x][coords.y + 2].PurifyWithBarrier();
-                goto case 3;
-            case 3:
-                if (coords.x - 1 >= 0 && coords.y - 1 >= 0 && tileMap[coords.x - 1][coords.y - 1] != null)
-                    tileMap[coords.x - 1][coords.y - 1].PurifyWithBarrier();
-                if (coords.x - 1 >= 0 && coords.y + 1 < mapBounds.y && tileMap[coords.x - 1][coords.y + 1] != null)
-                    tileMap[coords.x - 1][coords.y + 1].PurifyWithBarrier();
-                if (coords.x + 1 < mapBounds.x && coords.y - 1 >= 0 && tileMap[coords.x + 1][coords.y - 1] != null)
-                    tileMap[coords.x + 1][coords.y - 1].PurifyWithBarrier();
-                if (coords.x + 1 < mapBounds.x && coords.y + 1 < mapBounds.y && tileMap[coords.x + 1][coords.y + 1] != null)
-                    tileMap[coords.x + 1][coords.y + 1].PurifyWithBarrier();
-                goto case 2;
-            case 2:
-                if (coords.x - 1 >= 0 && tileMap[coords.x - 1][coords.y] != null) tileMap[coords.x - 1][coords.y].PurifyWithBarrier();
-                if (coords.x + 1 < mapBounds.x && tileMap[coords.x + 1][coords.y] != null) tileMap[coords.x + 1][coords.y].PurifyWithBarrier();
-                goto case 1;
-            case 1:
-                if (coords.y - 1 >= 0 && tileMap[coords.x][coords.y - 1] != null) tileMap[coords.x][coords.y - 1].PurifyWithBarrier();
-                if (coords.y + 1 < mapBounds.y && tileMap[coords.x][coords.y + 1] != null) tileMap[coords.x][coords.y + 1].PurifyWithBarrier();
-                tileMap[coords.x][coords.y].PurifyWithBarrier();
-                DegradeAfterTrigger();
-                break;
-            default:
-                break;
+        for (int i = 0; i <= spiritLevel * AREAS_OF_EFFECT_PER_LEVEL; i++) {
+            areaOfEffect[i].GetComponent<SpiritOfWisdomEffect>().ExertEffect(currentCoords, mapBounds, tileMap);
         }
+
+        DegradeAfterTrigger();
     }
 }
---
+++ IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfWisdomEffect.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritOfWisdomEffect : SpiritEffect {
+
+    public override void PerformEffect(Tile target) {
+        target.PurifyWithBarrier();
+    }
+}
