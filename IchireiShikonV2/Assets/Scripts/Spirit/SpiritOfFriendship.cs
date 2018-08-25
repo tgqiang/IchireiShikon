81a73463c34f64e85162676ffc41073d09078957
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2018-08-25 17:48:36 +0800

    Implemented spirit-effect for Spirit of Friendship

--- IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfFriendship.cs
+++ IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfFriendship.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class SpiritOfFriendship : Spirit {
 
+    const int AREAS_OF_EFFECT = 8;
+    const int AREAS_OF_EFFECT_PER_LEVEL = 2;
+
+    [SerializeField]
+    SpiritOfFriendshipEffect[] areaOfEffect;
+
     public override void Start() {
         base.Start();
+        if (areaOfEffect.Length != AREAS_OF_EFFECT) {
+            throw new System.Exception("Array of areas of effect is of incorrect length.");
+        }
         spiritType = CustomEnums.SpiritType.FRIENDSHIP;
     }
 
-    public override void TriggerEffect() {
+    public override void ShowAreaOfEffect() {
+        for (int i = 0; i < spiritLevel * AREAS_OF_EFFECT_PER_LEVEL; i++) {
+            areaOfEffect[i].RevealEffectMarker();
+        }
+    }
 
+    public override void HideAreaOfEffect() {
+        for (int i = 0; i < spiritLevel * AREAS_OF_EFFECT_PER_LEVEL; i++) {
+            areaOfEffect[i].HideEffectMarker();
+        }
+    }
+
+    public override void TriggerEffect() {
+        Tile[][] tileMap = Tile.gameLevel.levelData.tileMap;
+        Vector2Int mapBounds = Tile.gameLevel.levelData.mapBounds;
+        Vector2Int currentCoords = CurrentLocation.tileCoords;
+
+        for (int i = 0; i < spiritLevel * AREAS_OF_EFFECT_PER_LEVEL; i++) {
+            areaOfEffect[i].ExertEffect(currentCoords, mapBounds, tileMap);
+        }
+
+        DegradeAfterTrigger();
     }
 }
---
+++ IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfFriendshipEffect.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritOfFriendshipEffect : SpiritEffect {
+
+    const int POSITION_OFFSET_X = -8;
+    const int POSITION_OFFSET_Y = 4;
+    ParticleSystem emitter;
+
+    void Awake() {
+        emitter = GetComponent<ParticleSystem>();
+        if (emitter.IsTNull()) {
+            throw new System.Exception("Particle emitter missing from Spirit of Friendship effect object.");
+        }
+    }
+
+    void Update() {
+        if (GetComponent<SpriteRenderer>().enabled) {
+            Vector2Int mapBounds = Tile.gameLevel.levelData.mapBounds;
+            int c = Mathf.FloorToInt(transform.position.x - POSITION_OFFSET_X);
+            int r = Mathf.FloorToInt(POSITION_OFFSET_Y - transform.position.y);
+
+            if (r.IsBetweenExclusive(0, mapBounds.x) && c.IsBetweenExclusive(0, mapBounds.y)) {
+                Tile t = Tile.gameLevel.levelData.tileMap[r][c];
+                if (t != null && t.IsVacant(GetComponentInParent<Mergeable>())) {
+                    if (!emitter.isEmitting) {
+                        emitter.Play();
+                    }
+                } else {
+                    if (emitter.isEmitting) {
+                        emitter.Stop();
+                    }
+                }
+            } else {
+                if (emitter.isEmitting) {
+                    emitter.Stop();
+                }
+            }
+        }
+    }
+
+    public void RevealEffectMarker() {
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    public void HideEffectMarker() {
+        emitter.Stop();
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    public override void PerformEffect(Tile target) {
+        if (target.IsVacant()) {
+            int objectLevel = UnityEngine.Random.Range(0, GetComponentInParent<Spirit>().SpiritLevel);
+            int objectType = 0;
+            if (objectLevel > 0) {
+                objectType = UnityEngine.Random.Range(0, Enum.GetNames(typeof(CustomEnums.SpiritType)).Length);
+                Spirit spawnedSpirit = FindObjectOfType<ObjectSpawner>().SpawnSpirit(objectType, objectLevel, transform.position).GetComponent<Spirit>();
+                spawnedSpirit.CurrentLocation = target;
+                Tile.PlaceOnTile(spawnedSpirit, target);
+            } else {
+                objectType = UnityEngine.Random.Range(0, Enum.GetNames(typeof(CustomEnums.SoulType)).Length);
+                Soul spawnedSoul = FindObjectOfType<ObjectSpawner>().SpawnSoul(objectType, transform.position).GetComponent<Soul>();
+                spawnedSoul.CurrentLocation = target;
+                Tile.PlaceOnTile(spawnedSoul, target);
+            }
+        }
+
+        target.Purify();
+    }
+}
