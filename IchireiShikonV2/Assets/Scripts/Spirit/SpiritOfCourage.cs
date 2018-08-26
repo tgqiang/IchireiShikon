8234457d7e351e57cf7b32bec8280165fe19f308
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2018-08-27 00:56:31 +0800

    Reworked Spirit of Courage effect's structure

--- IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfCourage.cs
+++ IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfCourage.cs
@@ -5,49 +5,29 @@ using UnityEngine;
 public class SpiritOfCourage : Spirit {
 
     [SerializeField]
-    SpriteRenderer areaOfEffect;
+    SpiritOfCourageEffect areaOfEffect;
 
     public override void Start() {
         base.Start();
         if (areaOfEffect == null) {
-            throw new System.Exception("Child SpriteRenderer component missing from Mergeable object.");
+            throw new System.Exception("SpiritOfCourageEffect component missing from Spirit of Courage.");
         }
         spiritType = CustomEnums.SpiritType.COURAGE;
     }
 
     public override void ShowAreaOfEffect() {
-        areaOfEffect.size = Vector2.one * (2 * spiritLevel + 1);
-        areaOfEffect.enabled = true;
+        areaOfEffect.ShowAreaOfEffect(spiritLevel);
     }
 
     public override void HideAreaOfEffect() {
-        areaOfEffect.enabled = false;
+        areaOfEffect.HideAreaOfEffect();
     }
 
     public override void TriggerEffect() {
         Tile[][] tileMap = Tile.gameLevel.levelData.tileMap;
         Vector2Int mapBounds = Tile.gameLevel.levelData.mapBounds;
-
-        int rowMin = Mathf.Max(0, CurrentLocation.tileCoords.x - spiritLevel);
-        int rowMax = Mathf.Min(mapBounds.x - 1, CurrentLocation.tileCoords.x + spiritLevel);
-        int colMin = Mathf.Max(0, CurrentLocation.tileCoords.y - spiritLevel);
-        int colMax = Mathf.Min(mapBounds.y - 1, CurrentLocation.tileCoords.y + spiritLevel);
-
-        for (int row = rowMin; row <= rowMax; row++) {
-            for (int col = colMin; col <= colMax; col++) {
-                Tile target = tileMap[row][col];
-                if (target != null) {
-                    target.Purify();
-                    if (CurrentLocation.tileCoords == new Vector2Int(row, col)) {
-                        DegradeAfterTrigger();
-                        continue;
-                    }
-                    if (!target.IsVacant()) {
-                        FindObjectOfType<ObjectSpawner>().RemoveObjectFromGame(target.objectOnTile.gameObject);
-                        target.objectOnTile = null;
-                    }
-                }
-            }
-        }
+        Vector2Int currentCoords = CurrentLocation.tileCoords;
+        areaOfEffect.ExertEffect(currentCoords, mapBounds, tileMap);
+        DegradeAfterTrigger();
     }
 }
---
+++ IchireiShikonV2/Assets/Scripts/Spirit/SpiritOfCourageEffect.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritOfCourageEffect : SpiritEffect {
+
+    SpriteRenderer effectMarker;
+    Tile effectCenter;
+
+    void Awake() {
+        effectMarker = GetComponent<SpriteRenderer>();
+        if (effectMarker == null) {
+            throw new System.Exception("Child SpriteRenderer component missing from Mergeable object.");
+        }
+    }
+
+    public void ShowAreaOfEffect(int spiritLevel) {
+        effectMarker.size = Vector2.one * (2 * spiritLevel + 1);
+        effectMarker.enabled = true;
+    }
+
+    public void HideAreaOfEffect() {
+        effectMarker.enabled = false;
+    }
+
+    public override void ExertEffect(Vector2Int centerCoords, Vector2Int mapBounds, Tile[][] tileMap) {
+        int spiritLevel = GetComponentInParent<Spirit>().SpiritLevel;
+        effectCenter = tileMap[centerCoords.x][centerCoords.y];
+
+        int rowMin = Mathf.Max(0, centerCoords.x - spiritLevel);
+        int rowMax = Mathf.Min(mapBounds.x - 1, centerCoords.x + spiritLevel);
+        int colMin = Mathf.Max(0, centerCoords.y - spiritLevel);
+        int colMax = Mathf.Min(mapBounds.y - 1, centerCoords.y + spiritLevel);
+
+        for (int row = rowMin; row <= rowMax; row++) {
+            for (int col = colMin; col <= colMax; col++) {
+                Tile target = tileMap[row][col];
+                if (target != null) {
+                    PerformEffect(target);
+                }
+            }
+        }
+    }
+
+    public override void PerformEffect(Tile target) {
+        if (GetComponentInParent<Spirit>().IsTainted) {
+            target.Taint();
+        } else {
+            target.Purify();
+            if (target != effectCenter && !target.IsVacant()) {
+                Tile.RemoveFromTileAndDestroy(target.objectOnTile, target);
+            }
+        }
+    }
+}
